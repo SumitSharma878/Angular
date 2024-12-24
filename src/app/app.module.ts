9fe79637340928098c9cde76f83c64a1abf1f3d7
sumit Sharma <sumit@gmail.com>
2024-12-24 17:00:57 +0530

    images added in assests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BindingComponent } from './Lecture9/binding/binding.component';
-import { FormsModule} from '@angular/forms'
+import { FormsModule} from '@angular/forms';
+import { DropdownComponent } from './Lecture12/dropdown/dropdown.component'
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    BindingComponent
+    BindingComponent,
+    DropdownComponent
   ],
   imports: [
     BrowserModule,
