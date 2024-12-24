6c036880f540182d5ff8421414499beefe23c042
sumit Sharma <sumit@gmail.com>
2024-12-24 16:56:01 +0530

    created table and added bootstrap ,created update delete button , S.No,productId,productName,ProductImage,productprice

--- src/app/Lecture12/dropdown/dropdown.component.ts
+++ src/app/Lecture12/dropdown/dropdown.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./dropdown.component.css']
 })
 export class DropdownComponent {
- sumit:string="";
+ products=[
+  {
+    'id': '100',
+    'name':'item1',
+    'img':'../assets/img1.jpeg',
+    'price':40
+  },
+  {
+    'id': '101',
+    'name':'item2',
+    'img':'../assets/img2.jpeg',
+    'price':50
+  },
+  {
+    'id': '102',
+    'name':'item3',
+    'img':'../assets/img3.jpeg',
+    'price':69
+  },
+  {
+    'id': '103',
+    'name':'item4',
+    'img':'../assets/img4.webp',
+    'price':470
+  },
+  {
+    'id': '104',
+    'name':'item5',
+    'img':'../assets/img5.webp',
+    'price':405
+  },
+  {
+    'id': '105',
+    'name':'item6',
+    'img':'../assets/img6.webp',
+    'price':4025
+  }
+ ]
+
+
 }
